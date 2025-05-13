df60fb483c8954f6c0f4e582e9b650f28b2befed
Henry Lhimpton Thimpson Olivas <henrythimpson90@gmail.com>
2025-05-13 14:33:13 -0600

    Agregado el método para mostrar mis datos

--- EvaluacionGitHub/Program.cs
+++ EvaluacionGitHub/Program.cs
@@ -5,6 +5,16 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Proyecto de Ventas");
+           
         }
-    }
-}
+
+           internal void MostrarDatos()
+        {
+         
+            Console.WriteLine("Nombre:Henry Lhimpton Thimpson Olivas, Carnet:24509454");
+
+
+        }
+     }
+ }
+
